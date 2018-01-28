eed2a94c6f83a35590510e113a2091d2b50d306a
Patrick Sandoval <patsand24csu@yahoo.com>
2018-01-28 14:55:14 -0800

    test heroku

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-var uri = 'mongodb://patsand24csu:sammy1913@ds117878.mlab.com:17878/heroku_c7xbpzdb';
+var uri = 'mongodb://<dbuser>:<dbpassword>@ds117878.mlab.com:17878/heroku_c7xbpzdb';
 
 mongoose.connect(uri);
 mongoose.Promise = global.Promise;
